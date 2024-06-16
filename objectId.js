02fb1998ac7367f3e45f321214e9d64dae1a679f
Chris Axxwell <chrisaxxwell@gmail.com>
2024-06-16 18:09:59 -0300

    package.json updates

--- objectId.js
+++ objectId.js
@@ -1,26 +1,26 @@
 function ObjectId() {
-    if (!(this instanceof ObjectId)) {
-        return new ObjectId();
-    }
-    return this;
+   if (!(this instanceof ObjectId)) {
+      return new ObjectId();
+   }
+   return this;
 }
 
 function generate(counter) {
-    var timestamp = Math.floor(Date.now() / 1000).toString(16);
-    var machineIdentifier = btoa(navigator.userAgent).substring(0, 6);
-    var processId = Math.floor(Math.random() * 65535).toString(16).padStart(4, '0');
-    counter = (counter + 1) % 16777215;
-    var counterString = counter.toString(16).padStart(6, '0');
+   var timestamp = Math.floor(Date.now() / 1000).toString(16);
+   var machineIdentifier = btoa(navigator.userAgent).substring(0, 6);
+   var processId = Math.floor(Math.random() * 65535).toString(16).padStart(4, '0');
+   counter = (counter + 1) % 16777215;
+   var counterString = counter.toString(16).padStart(6, '0');
 
-    return timestamp + machineIdentifier + processId + counterString;
+   return timestamp + machineIdentifier + processId + counterString;
 }
 
 /** 
  * @returns {String} Generates a new unique 'ObjectId'.
  */
 ObjectId.prototype.toString = function () {
-    let counter = Math.floor(Math.random() * 16777215);
-    return generate(counter)
+   let counter = Math.floor(Math.random() * 16777215);
+   return generate(counter)
 }
 
 /** 
@@ -28,15 +28,15 @@ ObjectId.prototype.toString = function () {
 * @returns {String} Generates a new unique 'ObjectId' with free size.
  */
 ObjectId.prototype.unlimited = function (size) {
-    size = size || 24;
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
+   size = size || 24;
+   var result = '';
+   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
 
-    for (var i = 0; i < size; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+   for (var i = 0; i < size; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
 }
 
 export default ObjectId;
